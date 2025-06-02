8d2f3e14e99a04df75e57eab314c0b6288271ff7
mckinlde <douglas.e.mckinley@gmail.com>
2025-06-02 10:02:17 -0700

    ToDo: use an llm to read git diff and generate a better commit msg

--- nrs.sh
+++ nrs.sh
@@ -1,4 +1,5 @@
 sudo nixos-rebuild switch -I nixos-config=./configuration.nix
 git add .
+# ToDo: use an LLM to read git diff and generate a better commit msg
 git commit -m "run ./nrs.sh"
 git push
