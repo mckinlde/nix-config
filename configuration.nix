40d2965d1750663720ef2f8076e24022732d3316
mckinlde <douglas.e.mckinley@gmail.com>
2025-06-05 14:39:24 -0700

    run ./nrs.sh

--- configuration.nix
+++ configuration.nix
@@ -118,6 +118,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    zellij # terminal multiplexer; tmux but better
     git # local git
     gh # online github (ssh tools)
     git-lfs # git large file storage
